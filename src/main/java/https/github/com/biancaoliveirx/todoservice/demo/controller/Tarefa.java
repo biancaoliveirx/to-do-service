3652a4f9205a8186c70dc03aedf69b7394d67c21
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-10-26 16:41:44 -0300

    criando um teste

---
+++ src/main/java/https/github/com/biancaoliveirx/todoservice/demo/controller/Tarefa.java
@@ -0,0 +1,11 @@
+package https.github.com.biancaoliveirx.todoservice.demo.controller;
+
+public class Tarefa {
+    private String id;
+    private String descricao;
+    private String vencimento;
+    private String estado;
+
+
+    
+}
---
+++ src/main/java/https/github/com/biancaoliveirx/todoservice/demo/controller/TarefaController.java
@@ -0,0 +1,13 @@
+package https.github.com.biancaoliveirx.todoservice.demo.controller;
+
+import java.util.LinkedList;
+
+public class TarefaController {
+    private LinkedList<Tarefa> listaTarefa;
+    public TarefaController() {
+        this.listaTarefa = new LinkedList<>();
+    }
+
+
+
+}
